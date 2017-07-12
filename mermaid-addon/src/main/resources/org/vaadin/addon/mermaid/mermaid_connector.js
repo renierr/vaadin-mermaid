bac1b333375c9ec9d297958ca5816d8f91646815
Renier <renierr@users.noreply.github.com>
2017-07-12 14:14:21 +0200

    netter IE detection

--- mermaid-addon/src/main/resources/org/vaadin/addon/mermaid/mermaid_connector.js
+++ mermaid-addon/src/main/resources/org/vaadin/addon/mermaid/mermaid_connector.js
@@ -6,7 +6,29 @@ window.org_vaadin_addon_mermaid_MermaidChart = function() {
   var id = 'mermaidChart';
   var callListener = false;
 
-  if (navigator.userAgent.search("MSIE") >= 0) {
+  function detectIE() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+      // IE 10 or older => return version number
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+      // IE 11 => return version number
+      var rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+      // Edge (IE 12+) => return version number
+      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+    // other browser
+    return false;
+  }
+
+  if (!(detectIE() === false)) {
     Object.defineProperty(SVGElement.prototype, 'outerHTML', {
       get: function () {
         var $node, $temp;
